a2cab3950367a14c3f55b6919b62108a2b39549d
SenoussiAl <84295873+SenoussiAl@users.noreply.github.com>
2025-02-22 13:46:50 -0500

    Added Experience Component

--- my-portfolio/src/components/Experience.js
+++ my-portfolio/src/components/Experience.js
@@ -1,26 +1,33 @@
 import React from 'react';
+import '../styles/Experience.css'; // Import the CSS file
 
 const Experience = () => {
     return (
         <section id="experience">
             <h2>Experience</h2>
-            <div>
-                <h3>Configuration Management Intern</h3>
-                <p>Shared Services Canada | May 2024 - August 2024</p>
-                <ul>
-                    <li>Optimized configuration processes by mapping thousands of servers to applications.</li>
-                    <li>Enhanced documentation workflows, leading to a 25% improvement in efficiency.</li>
-                    <li>Designed CI/CD pipelines, achieving a 40% reduction in deployment times.</li>
-                </ul>
-            </div>
-            <div>
-                <h3>Frontend Developer Intern</h3>
-                <p>Service Canada | September 2022 - December 2022</p>
-                <ul>
-                    <li>Revitalized web UI using React.js, Node.js, and TypeScript.</li>
-                    <li>Developed dynamic, data-driven web pages for real-time analytics visualization.</li>
-                    <li>Enhanced backend integration, reducing API response times.</li>
-                </ul>
+            <div className="timeline">
+                <div className="container left">
+                    <div className="content">
+                        <h3>Configuration Management</h3>
+                        <h4>Shared Services Canada | May 2024 - August 2024</h4>
+                        <p>Optimized configuration processes by mapping thousands of servers to applications.</p>
+                    </div>
+                </div>
+
+                <div className="container right">
+                <div className="content">
+                        <h3>Frontend Developer</h3>
+                        <h4>Royal Canadian Mounted Police | January 2022 - July 2022</h4>
+                        <p>Optimized configuration processes by mapping thousands of servers to applications.</p>
+                    </div>
+                </div>
+                <div className="container left">
+                    <div className="content">
+                        <h3>Technical Analyst</h3>
+                        <h4>Royal Canadian Mounted Police | January 2022 - July 2022</h4>
+                        <p>Optimized configuration processes by mapping thousands of servers to applications.</p>
+                    </div>
+                </div>
             </div>
         </section>
     );
