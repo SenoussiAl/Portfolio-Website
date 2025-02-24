3b7174d2e4ada2cc597ba334b5dae97827a45bd9
SenoussiAl <84295873+SenoussiAl@users.noreply.github.com>
2025-02-23 19:44:29 -0500

    Modified Navbar

--- my-portfolio/src/index.js
+++ my-portfolio/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client'
+import ReactDOM from 'react-dom/client'; 
 import App from './App';
 import './index.css';
 
-// Use createRoot instead of ReactDOM.render
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
