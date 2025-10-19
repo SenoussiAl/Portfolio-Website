23686b5f2feb102a6ec8e6d374d85f3cccc0ae1e
Senoussi Al-Nadjib <84295873+SenoussiAl@users.noreply.github.com>
2025-10-19 14:31:26 -0400

    Add C# and ASP.NET to Tech Stack component

--- my-portfolio/src/components/TechStack.js
+++ my-portfolio/src/components/TechStack.js
@@ -1,43 +1,41 @@
 // TechStack.js
 import React from 'react';
 import '../styles/TechStack.css';
-import { FaJava, FaPython, FaReact, FaNodeJs, FaHtml5, FaCss3Alt, FaGitAlt } from 'react-icons/fa';
-import { SiCplusplus, SiSpringboot, SiPostman, SiPostgresql, SiTypescript, SiJavascript, } from 'react-icons/si';
-
+import { FaJava, FaPython, FaReact, FaHtml5, FaCss3Alt, FaGitAlt } from 'react-icons/fa';
+import { SiCplusplus, SiSpringboot, SiPostgresql, SiTypescript, SiJavascript, SiDotnet, SiCsharp } from 'react-icons/si';
 
 const TechStack = () => {
     const techStackData = [
         { name: 'C++', icon: <SiCplusplus className="tech-stack-icon" /> },
         { name: 'Java', icon: <FaJava className="tech-stack-icon" /> },
         { name: 'Python', icon: <FaPython className="tech-stack-icon" /> },
+        { name: 'C#', icon: <SiCsharp className="tech-stack-icon" /> },
+        { name: 'ASP.NET', icon: <SiDotnet className="tech-stack-icon" /> },
         { name: 'JavaScript', icon: <SiJavascript className="tech-stack-icon" /> },
         { name: 'HTML', icon: <FaHtml5 className="tech-stack-icon" /> },
         { name: 'CSS', icon: <FaCss3Alt className="tech-stack-icon" /> },
         { name: 'SQL', icon: <SiPostgresql className="tech-stack-icon" /> },
         { name: 'TypeScript', icon: <SiTypescript className="tech-stack-icon" /> },
         { name: 'React.js', icon: <FaReact className="tech-stack-icon" /> },
-        { name: 'Node.js', icon: <FaNodeJs className="tech-stack-icon" /> },
-        { name: 'SpringBoot', icon: <SiSpringboot className="tech-stack-icon" /> },
-        { name: 'Postman', icon: <SiPostman className="tech-stack-icon" /> },
+        { name: 'Spring Boot', icon: <SiSpringboot className="tech-stack-icon" /> },
         { name: 'Git', icon: <FaGitAlt className="tech-stack-icon" /> },
     ];
 
     return (
         <section id="techstack">
-        <div className="tech-stack-container">
-            <h2 className="tech-stack-title">Tech Stack</h2>
-            <div className="tech-stack-grid">
-                {techStackData.map((tech, index) => (
-                    <div key={index} className="tech-stack-item">
-                        <div className="tech-stack-item-inner">
-                            {tech.icon}
-                          {/*  <span className="tech-stack-name">{tech.name}</span>
-                        */}
+            <div className="tech-stack-container">
+                <h2 className="tech-stack-title">Tech Stack</h2>
+                <div className="tech-stack-grid">
+                    {techStackData.map((tech, index) => (
+                        <div key={index} className="tech-stack-item">
+                            <div className="tech-stack-item-inner">
+                                {tech.icon}
+                                {/* <span className="tech-stack-name">{tech.name}</span> */}
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
-        </div>
         </section>
     );
 };
