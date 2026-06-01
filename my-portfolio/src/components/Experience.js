4826f4e69966b650f5ccaefab52cb1c3e3196bd8
Senoussi Al-Nadjib <84295873+SenoussiAl@users.noreply.github.com>
2026-06-01 15:42:31 -0400

    Remove unused useEffect import in Experience.js

--- my-portfolio/src/components/Experience.js
+++ my-portfolio/src/components/Experience.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useInView } from 'react-intersection-observer';
 import '../styles/Experience.css';
 
